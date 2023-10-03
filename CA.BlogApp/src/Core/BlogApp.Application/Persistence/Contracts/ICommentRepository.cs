589e9d0f875ecf27ba68f30ba65d0fd02772e426
Fekede Alemayehu <fekedealemayehu4@gmail.com>
2023-10-03 15:43:20 +0300

    Add: Day 10 Task on clean architecture

---
+++ CA.BlogApp/src/Core/BlogApp.Application/Persistence/Contracts/ICommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlogApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Persistence.Contracts
+{
+    public interface ICommentRepository : IGenericRepository<Comment>
+    {
+        Task<T> GetComments<T>(int id);
+    }
+}
---
+++ CA.BlogApp/src/Core/BlogApp.Application/Persistence/Contracts/IPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.Application.Persistence.Contracts
+{
+    public interface IPostRepository : IGenericRepository<Post>    
+    {
+
+    }
+}
